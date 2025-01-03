3bb6e47b2f590260f650741df4e7dd6ee4f6f711
kanakana-rcj <nakagawa.dadada5@gmail.com>
2025-01-03 14:10:45 +0900

    support windows

---
+++ src/main.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'main.exe' with Python DLL being found.
+set PATH=c:\users\81807\appdata\local\programs\python\python312;%PATH%
+set PYTHONHOME=C:\Users\81807\AppData\Local\Programs\Python\Python312
+set NUITKA_PYTHONPATH=C:\Users\81807\Desktop\BlockFrontManager\src;C:\Users\81807\AppData\Local\Programs\Python\Python312\DLLs;C:\Users\81807\AppData\Local\Programs\Python\Python312\Lib;C:\Users\81807\AppData\Local\Programs\Python\Python312;C:\Users\81807\Desktop\BlockFrontManager\win_venv;C:\Users\81807\Desktop\BlockFrontManager\win_venv\Lib\site-packages
+"%~dp0main.exe" %*
